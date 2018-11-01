5b05b2669ef845e24a5609186a9080cecd1b845e
Max Bruce <Protryon@users.noreply.github.com>
2018-11-01 14:24:38 -0700

    Prevent minimal-codegen from creating HTML comments (#72)

--- test/test262/expectations.js
+++ test/test262/expectations.js
@@ -3,8 +3,5 @@
 module.exports = {
   xfail: [
 
-    // html comments: https://github.com/shapesecurity/shift-codegen-js/issues/68
-    '4c3a394af4d281d1.js-tree.json',
-    'f96c694c5a2f2be9.js-tree.json',
   ],
 };
