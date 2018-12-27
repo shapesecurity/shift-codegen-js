38974d30201ed1966a721d0a965dac2d75f473b5
mbruce <max.bruce12@gmail.com>
2018-12-27 12:07:11 -0800

    update dependencies

--- test/test262/expectations.js
+++ test/test262/expectations.js
@@ -5,5 +5,10 @@ module.exports = {
     // invalid test: https://github.com/tc39/test262-parser-tests/issues/19
     '3dbb6e166b14a6c0.js-tree.json',
 
+    // invalid test: https://github.com/shapesecurity/shift-parser-expectations/issues/4
+    '66e383bfd18e66ab.js-tree.json',
+    'e4a43066905a597b.js-tree.json',
+    'bf49ec8d96884562.js-tree.json',
+    '78c215fabdf13bae.js-tree.json',
   ],
 };
